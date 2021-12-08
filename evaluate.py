59826d1d736d06dd5bbe35051c8b63a36e20a2e6
nelson870708 <nelson870708@gmail.com>
2021-12-08 23:20:58 +0800

    Fix bug: five cropped image should be concatenated together

--- evaluate.py
+++ evaluate.py
@@ -41,13 +41,14 @@ def evaluate_phase1(dataloader, model, loss, dataset_size, device=torch.device('
         categories = categories.to(device)
 
         # Format batch
-        bs = ref_imgs.size(0)
+        bs, ncrops, c, h, w = ref_imgs.size()
 
         with torch.no_grad():
-            _, pred_categories, pred_scores = model['netD'](ref_imgs, dist_imgs)
+            _, pred_categories, pred_scores = model['netD'](ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
+            pred_scores_avg = pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
 
             record['errD_real_adv'] = loss['ce_loss'](pred_categories, categories).item()
-            record['errD_real_clf'] = loss['mse_loss'](pred_scores, scores).item()
+            record['errD_real_clf'] = loss['mse_loss'](pred_scores_avg, scores).item()
 
         # Record original scores and predict scores
         record['gt_scores'].append(origin_scores)
