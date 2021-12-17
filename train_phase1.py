bfbc2c8f1d7e787b8c89bb9f50338f82f743b3bd
nelson870708 <nelson870708@gmail.com>
2021-12-17 19:24:09 +0800

    Code Refactor: format code

--- train_phase1.py
+++ train_phase1.py
@@ -14,9 +14,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 from src.config.config_phase1 import get_cfg_defaults
 from src.data.dataset import create_dataloaders
-from src.modeling.module import Generator, MultiTask
 from src.modeling.evaluate import evaluate_phase1
 from src.modeling.log import write_epoch_log
+from src.modeling.module import Generator, MultiTask
 from src.tool.train import train_phase1
 
 torch.multiprocessing.set_sharing_strategy('file_system')
