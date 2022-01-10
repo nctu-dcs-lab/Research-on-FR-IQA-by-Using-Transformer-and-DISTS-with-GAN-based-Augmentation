764bd9a379202cff7436dca2ead0d74dc3e4bc69
nelson870708 <nelson870708@gmail.com>
2022-01-10 09:57:51 +0800

    Fix Bug: typo error

--- src/tool/trainer.py
+++ src/tool/trainer.py
@@ -416,7 +416,7 @@ class TrainerPhase2(Trainer):
                 # Generate batch of latent vectors
                 noise = torch.randn(bs, self.latent_dim, device=self.device)
 
-                fake_imgs = self.netD(ref_imgs,
+                fake_imgs = self.netG(ref_imgs,
                                       noise,
                                       scores.view(bs, -1),
                                       categories.view(bs, -1).float())
@@ -468,7 +468,7 @@ class TrainerPhase2(Trainer):
         self.netG.eval()
         self.netD.eval()
 
-        with tqdm(self.dataloaders['train']) as tepoch:
+        with tqdm(self.dataloaders['val']) as tepoch:
             for iteration, (ref_imgs, dist_imgs, scores, categories, origin_scores) in enumerate(tepoch):
                 ref_imgs = ref_imgs.to(self.device)
                 dist_imgs = dist_imgs.to(self.device)
@@ -496,7 +496,7 @@ class TrainerPhase2(Trainer):
                     """
                     noise = torch.randn(bs, self.latent_dim, device=self.device)
 
-                    fake_imgs = self.netD(
+                    fake_imgs = self.netG(
                         ref_imgs.view(-1, c, h, w),
                         noise.repeat_interleave(ncrops, dim=0),
                         scores.repeat_interleave(ncrops).view(bs * ncrops, -1),
