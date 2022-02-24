1d31edd00dc360ac0febd0d5253ea4ec86c3dd14
nelson870708 <nelson870708@gmail.com>
2022-02-24 19:52:34 +0800

    Modify: get predict scores from file, not evaluate it again and again

--- 3.0.3.py
+++ 3.0.3.py
@@ -1,121 +1,37 @@
-from pathlib import Path
+import pickle
 
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+import numpy as np
 
-from src.config.config import get_cfg_defaults
-from src.data.dataset import PIPAL
-from src.modeling.module import MultiTask
 from src.tool.evaluate import calculate_correlation_coefficient
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-batch_size = 16
-num_workers = 6
+pred_scores = []
+pred_scores_path = [
+    'scores_record/1.1.7 pred_scores.pickle',
+    'scores_record/4.0.0 pred_scores.pickle',
+]
 
-# Dataset
-datasets = {
-    x: PIPAL(root_dir=Path('../data/PIPAL(processed)'),
-             mode=x)
-    for x in ['train', 'val', 'test']
+with open('scores_record/gt_scores.pickle', 'rb') as handle:
+    gt_scores = pickle.load(handle)
+
+for path in pred_scores_path:
+    with open(path, 'rb') as handle:
+        pred_scores.append(pickle.load(handle))
+
+pred_scores = tuple(pred_scores)
+
+avg_pred_scores = {
+    'val': np.zeros(pred_scores[0]['val'].size),
+    'test': np.zeros(pred_scores[0]['test'].size)
 }
 
-datasets_size = {x: len(datasets[x]) for x in ['train', 'val', 'test']}
+for mode in ['val', 'test']:
+    for scores in pred_scores:
+        avg_pred_scores[mode] += scores[mode]
+    avg_pred_scores[mode] /= len(pred_scores)
 
-# DataLoader
-dataloaders = {
-    x: DataLoader(datasets[x],
-                  batch_size=batch_size,
-                  shuffle=True,
-                  num_workers=num_workers)
-    for x in ['train', 'val', 'test']
-}
+    plcc, srcc, krcc = calculate_correlation_coefficient(
+        gt_scores[mode],
+        avg_pred_scores[mode]
+    )
 
-low_level_cfg = get_cfg_defaults()
-low_level_cfg.merge_from_file('src/config/experiments/1.1.7_config.yaml')
-
-dists_cfg = get_cfg_defaults()
-dists_cfg.merge_from_file('src/config/experiments/4.0.0_config.yaml')
-
-low_level_cfg.freeze()
-dists_cfg.freeze()
-
-low_level_iqt = MultiTask(low_level_cfg).to(device)
-low_level_iqt.load_state_dict(torch.load('experiments/1.1.7/models/netD_epoch79.pth'))
-for parameter in low_level_iqt.parameters():
-    parameter.requires_grad = False
-low_level_iqt.eval()
-
-dists = MultiTask(dists_cfg).to(device)
-dists.load_state_dict(torch.load('experiments/4.0.0/models/netD_epoch113.pth'))
-for parameter in dists.parameters():
-    parameter.requires_grad = False
-dists.eval()
-
-criterion = nn.MSELoss()
-
-running_loss = 0
-gt_scores = []
-pred_scores = []
-
-for ref_imgs, dist_imgs, scores, categories, origin_scores in tqdm(dataloaders['val']):
-    ref_imgs = ref_imgs.to(device)
-    dist_imgs = dist_imgs.to(device)
-    scores = scores.to(device).float()
-
-    # Format batch
-    bs, ncrops, c, h, w = ref_imgs.size()
-
-    _, _, low_level_pred_scores = low_level_iqt(ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
-    low_level_pred = low_level_pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
-
-    _, _, dists_pred_scores = dists(ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
-    dists_pred = dists_pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
-
-    final_pred = (low_level_pred + dists_pred) / 2
-
-    loss = criterion(final_pred.view(-1), scores)
-
-    # statistics
-    running_loss += loss.item() * ref_imgs.size(0)
-    gt_scores.append(origin_scores)
-    pred_scores.append(final_pred.cpu().detach())
-
-epoch_loss = running_loss / datasets_size['val']
-
-plcc, srcc, krcc = calculate_correlation_coefficient(
-    torch.cat(gt_scores).numpy(),
-    torch.cat(pred_scores).numpy()
-)
-
-print(f'val Loss: {epoch_loss}')
-print(f'val PLCC: {plcc}, SRCC: {srcc}, KRCC: {krcc}')
-
-gt_scores = []
-pred_scores = []
-
-for ref_imgs, dist_imgs, _, _, origin_scores in tqdm(dataloaders['test']):
-    ref_imgs = ref_imgs.to(device)
-    dist_imgs = dist_imgs.to(device)
-
-    # Format batch
-    bs, ncrops, c, h, w = ref_imgs.size()
-
-    _, _, low_level_pred_scores = low_level_iqt(ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
-    low_level_pred = low_level_pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
-
-    _, _, dists_pred_scores = dists(ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
-    dists_pred = dists_pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
-
-    final_pred = (low_level_pred + dists_pred) / 2
-
-    gt_scores.append(origin_scores)
-    pred_scores.append(final_pred.cpu().detach())
-
-plcc, srcc, krcc = calculate_correlation_coefficient(
-    torch.cat(gt_scores).numpy(),
-    torch.cat(pred_scores).numpy()
-)
-
-print(f'test PLCC: {plcc}, SRCC: {srcc}, KRCC: {krcc}')
+    print(f'{mode} PLCC: {plcc: .4f}, SRCC: {srcc: .4f}, KRCC: {krcc: .4f}')
--- 3.0.8.py
+++ 3.0.8.py
@@ -4,39 +4,37 @@ import numpy as np
 
 from src.tool.evaluate import calculate_correlation_coefficient
 
-results = []
-result_path = [
-    'pred_scores/1.1.7 pred_scores.pickle',
-    'pred_scores/1.3.2 pred_scores.pickle',
-    'pred_scores/1.5.1 pred_scores.pickle',
-    'pred_scores/2.2.3.2 pred_scores.pickle',
-    'pred_scores/4.0.0 pred_scores.pickle'
+pred_scores = []
+pred_scores_path = [
+    'scores_record/1.1.7 pred_scores.pickle',
+    'scores_record/1.3.2 pred_scores.pickle',
+    'scores_record/1.5.1 pred_scores.pickle',
+    'scores_record/4.0.0 pred_scores.pickle',
+    'scores_record/2.2.3.2 pred_scores.pickle',
 ]
 
-for path in result_path:
+with open('scores_record/gt_scores.pickle', 'rb') as handle:
+    gt_scores = pickle.load(handle)
+
+for path in pred_scores_path:
     with open(path, 'rb') as handle:
-        results.append(pickle.load(handle))
+        pred_scores.append(pickle.load(handle))
 
-results = tuple(results)
+pred_scores = tuple(pred_scores)
 
-gt_scores = {
-    'val': results[0]['val']['gt_scores'],
-    'test': results[0]['test']['gt_scores']
-}
-
-avg_pred = {
-    'val': np.zeros(results[0]['val']['pred_scores'].size),
-    'test': np.zeros(results[0]['test']['pred_scores'].size)
+avg_pred_scores = {
+    'val': np.zeros(pred_scores[0]['val'].size),
+    'test': np.zeros(pred_scores[0]['test'].size)
 }
 
 for mode in ['val', 'test']:
-    for result in results:
-        avg_pred[mode] += result[mode]['pred_scores']
-    avg_pred[mode] /= len(results)
+    for scores in pred_scores:
+        avg_pred_scores[mode] += scores[mode]
+    avg_pred_scores[mode] /= len(pred_scores)
 
     plcc, srcc, krcc = calculate_correlation_coefficient(
         gt_scores[mode],
-        avg_pred[mode]
+        avg_pred_scores[mode]
     )
 
     print(f'{mode} PLCC: {plcc: .4f}, SRCC: {srcc: .4f}, KRCC: {krcc: .4f}')
