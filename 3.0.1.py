22dbd847ca6004476619e301201c8e4bfed448b2
nelson870708 <nelson870708@gmail.com>
2022-03-09 11:25:55 +0800

    Modify: feed scores record into linear layer to get the final result and tuning the hyperparameters using grid search

--- 3.0.1.py
+++ 3.0.1.py
@@ -1,115 +1,66 @@
-from pathlib import Path
+import pickle
 
-import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+import numpy as np
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.neural_network import MLPRegressor
 
-from src.config.config import get_cfg_defaults
-from src.data.dataset import PIPAL
-from src.modeling.module import MultiTask
+from src.tool.evaluate import calculate_correlation_coefficient
 
-num_epoch = 100
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-lr = 2e-4
-batch_size = 16
-num_workers = 6
+pred_scores = []
 
+pred_scores_path = [
+    'scores_record/1.1.7 pred_scores.pickle',
+    'scores_record/1.3.2 pred_scores.pickle',
+]
 
-class Regression(nn.Module):
-    def __init__(self):
-        super(Regression, self).__init__()
-        self.regression = nn.Linear(2, 1)
+with open('scores_record/gt_scores.pickle', 'rb') as handle:
+    gt_scores = pickle.load(handle)
 
-    def forward(self, x):
-        return self.regression(x)
+for path in pred_scores_path:
+    with open(path, 'rb') as handle:
+        pred_scores.append(pickle.load(handle))
 
+pred_scores = tuple(pred_scores)
 
-# Dataset
-datasets = {
-    x: PIPAL(root_dir=Path('../data/PIPAL(processed)'),
-             mode=x)
-    for x in ['train', 'val', 'test']
-}
+X = np.stack((np.concatenate((pred_scores[0]['train'], pred_scores[0]['val'])),
+              np.concatenate((pred_scores[1]['train'], pred_scores[1]['val'])))).T
+y = np.concatenate((gt_scores['train'], gt_scores['val']))
+test_fold = np.concatenate((np.zeros(gt_scores['train'].size), -np.ones(gt_scores['val'].size)))
 
-datasets_size = {x: len(datasets[x]) for x in ['train', 'val', 'test']}
+ps = PredefinedSplit(test_fold)
+print(ps.get_n_splits())
 
-# DataLoader
-dataloaders = {
-    x: DataLoader(datasets[x],
-                  batch_size=batch_size,
-                  shuffle=True,
-                  num_workers=num_workers)
-    for x in ['train', 'val', 'test']
-}
+X_val = np.stack((pred_scores[0]['val'], pred_scores[1]['val'])).T
+y_val = gt_scores['val']
 
-low_level_cfg = get_cfg_defaults()
-low_level_cfg.merge_from_file('src/config/experiments/1.1.7_config.yaml')
-medium_level_cfg = get_cfg_defaults()
-medium_level_cfg.merge_from_file('src/config/experiments/1.3.2_config.yaml')
+X_test = np.stack((pred_scores[0]['test'], pred_scores[1]['test'])).T
+y_test = gt_scores['test']
 
-low_level_cfg.freeze()
-medium_level_cfg.freeze()
+parameters = {
+    'solver': ('sgd', 'adam'),
+    'alpha': (0.001, 0.0001, 0.00001),
+    'learning_rate': ('constant', 'adaptive'),
+    'learning_rate_init': (0.01, 0.001, 0.0001)}
 
-low_level_iqt = MultiTask(low_level_cfg).to(device)
-low_level_iqt.load_state_dict(torch.load('experiments/1.1.7/models/netD_epoch79.pth'))
-for parameter in low_level_iqt.parameters():
-    parameter.requires_grad = False
-low_level_iqt.eval()
+mlp_regr = MLPRegressor(max_iter=500)
 
-medium_level_iqt = MultiTask(medium_level_cfg).to(device)
-medium_level_iqt.load_state_dict((torch.load('experiments/1.3.2/models/netD_epoch57.pth')))
-for parameter in medium_level_iqt.parameters():
-    parameter.requires_grad = False
-medium_level_iqt.eval()
+regr = GridSearchCV(mlp_regr, parameters, verbose=3, cv=ps)
+regr.fit(X, y)
 
-regression = Regression().to(device)
+X_val_pred = regr.predict(X_val)
 
-criterion = nn.MSELoss()
+plcc, srcc, krcc = calculate_correlation_coefficient(
+    y_val,
+    X_val_pred
+)
 
-optimizer = optim.Adam(regression.parameters(), lr=lr)
+print(f'val PLCC: {plcc: .4f}, SRCC: {srcc: .4f}, KRCC: {krcc: .4f}')
 
-for epoch in range(num_epoch):
-    print(f'Epoch {epoch + 1}/{num_epoch}')
-    print('-' * 10)
+X_test_pred = regr.predict(X_test)
 
-    for phase in ['train', 'val']:
+plcc, srcc, krcc = calculate_correlation_coefficient(
+    y_test,
+    X_test_pred
+)
 
-        if phase == 'train':
-            regression.train()
-        else:
-            regression.eval()
-
-        running_loss = 0
-
-        for ref_imgs, dist_imgs, scores, categories, origin_scores in tqdm(dataloaders[phase]):
-            ref_imgs = ref_imgs.to(device)
-            dist_imgs = dist_imgs.to(device)
-            scores = scores.to(device).float()
-
-            if phase == 'train':
-                _, _, low_level_pred = low_level_iqt(ref_imgs, dist_imgs)
-                _, _, medium_level_pred = medium_level_iqt(ref_imgs, dist_imgs)
-            else:
-                # Format batch
-                bs, ncrops, c, h, w = ref_imgs.size()
-                _, _, low_level_pred_scores = low_level_iqt(ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
-                low_level_pred = low_level_pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
-                _, _, medium_level_pred_scores = medium_level_iqt(ref_imgs.view(-1, c, h, w), dist_imgs.view(-1, c, h, w))
-                medium_level_pred = medium_level_pred_scores.view(bs, ncrops, -1).mean(1).view(-1)
-
-            with torch.set_grad_enabled(phase == 'train'):
-                final_pred = regression(torch.cat((low_level_pred.view(-1, 1), medium_level_pred.view(-1, 1)), 1))
-
-                loss = criterion(final_pred.view(-1), scores)
-
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-
-            # statistics
-            running_loss += loss.item() * ref_imgs.size(0)
-
-        epoch_loss = running_loss / datasets_size[phase]
-
-        print(f'{phase} Loss: {epoch_loss: .4f}')
+print(f'test PLCC: {plcc: .4f}, SRCC: {srcc: .4f}, KRCC: {krcc: .4f}')
