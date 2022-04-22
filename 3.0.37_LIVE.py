9c3e2e355ec2b731db66ee28982af7cb071c571c
nelson870708 <nelson870708@gmail.com>
2022-04-22 21:30:01 +0800

    Add Code: add code for weighted averaging predicted scores of LIVE dataset

---
+++ 3.0.37_LIVE.py
@@ -0,0 +1,38 @@
+import pickle
+
+import numpy as np
+
+from src.tool.evaluate import calculate_correlation_coefficient
+
+pred_scores = []
+pred_scores_path = [
+    'LIVE scores_record/1.1.7 pred_scores.pickle',
+    'LIVE scores_record/1.3.2 pred_scores.pickle',
+    'LIVE scores_record/1.5.1 pred_scores.pickle',
+    'LIVE scores_record/1.7.4 pred_scores.pickle',
+    'LIVE scores_record/2.1.3.3 pred_scores.pickle',
+    'LIVE scores_record/4.0.0 pred_scores.pickle',
+    'LIVE scores_record/5.1.0 pred_scores.pickle',
+]
+pred_scores_weight = (0.29443265, 0.02250836, 0.02398765, 0.24275188, 0.33650384, 0.05174801, 0.02806762)
+
+with open('LIVE scores_record/gt_scores.pickle', 'rb') as handle:
+    gt_scores = pickle.load(handle)
+
+for path in pred_scores_path:
+    with open(path, 'rb') as handle:
+        pred_scores.append(pickle.load(handle))
+
+pred_scores = tuple(pred_scores)
+
+weight_avg_pred_scores = np.zeros(pred_scores[0].size)
+
+for scores, weight in zip(pred_scores, pred_scores_weight):
+    weight_avg_pred_scores += scores * (weight / sum(pred_scores_weight))
+
+plcc, srcc, krcc = calculate_correlation_coefficient(
+    gt_scores,
+    weight_avg_pred_scores
+)
+
+print(f'PLCC: {plcc: .4f}, SRCC: {srcc: .4f}, KRCC: {krcc: .4f}')
