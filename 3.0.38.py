6bdf3d82eedb491185efc9cf8d060be26aaaaa91
nelson870708 <nelson870708@gmail.com>
2022-04-24 13:11:32 +0800

    Add Files: add experiment 3.0.38, 3.0.39 and 3.0.40 for PIPAL dataset and LIVE dataset

---
+++ 3.0.38.py
@@ -0,0 +1,71 @@
+import pickle
+import time
+
+import numpy as np
+from scipy.optimize import differential_evolution
+
+from src.tool.evaluate import calculate_correlation_coefficient
+
+
+def loss_function(weights):
+    plcc, srcc, krcc = calculate_correlation_coefficient(
+        gt_scores['val'],
+        np.average(stack_pred_scores['val'], axis=0, weights=weights)
+    )
+    score = plcc + srcc + krcc
+    return 3 - score
+
+
+pred_scores = []
+pred_scores_path = [
+    'scores_record/1.1.7 pred_scores.pickle',
+    'scores_record/1.3.2 pred_scores.pickle',
+    'scores_record/1.5.1 pred_scores.pickle',
+    'scores_record/1.7.4 pred_scores.pickle',
+    'scores_record/2.2.3.3 pred_scores.pickle',
+    'scores_record/4.0.0 pred_scores.pickle',
+    'scores_record/5.1.0 pred_scores.pickle',
+]
+
+with open('scores_record/gt_scores.pickle', 'rb') as handle:
+    gt_scores = pickle.load(handle)
+
+for path in pred_scores_path:
+    with open(path, 'rb') as handle:
+        pred_scores.append(pickle.load(handle))
+
+pred_scores = tuple(pred_scores)
+
+stack_pred_scores = {
+    'train': np.stack([pred_scores[idx]['train'] for idx in range(len(pred_scores))]),
+    'val': np.stack([pred_scores[idx]['val'] for idx in range(len(pred_scores))]),
+    'test': np.stack([pred_scores[idx]['test'] for idx in range(len(pred_scores))])
+}
+
+bound_w = [(0.0, 1.0) for _ in range(len(pred_scores))]
+
+begin_time = time.time()
+result = differential_evolution(loss_function, bound_w, maxiter=1000, tol=1e-7, disp=True, workers=-1)
+
+best_weights = result['x']
+print(f'Optimized Weights: {best_weights}')
+
+# evaluate chosen weights
+plcc, srcc, krcc = calculate_correlation_coefficient(
+    gt_scores['val'],
+    np.average(stack_pred_scores['val'], axis=0, weights=best_weights)
+)
+score = plcc + srcc + krcc
+print(f'Optimized Weights Score: {score:.3f}')
+
+end_time = time.time()
+
+print(f'Execution time: {int((end_time - begin_time) // 3600)}:{int((end_time - begin_time) // 60)}:{int((end_time - begin_time) % 60)}')
+
+for mode in ['train', 'val', 'test']:
+    plcc, srcc, krcc = calculate_correlation_coefficient(
+        gt_scores[mode],
+        np.average(stack_pred_scores[mode], axis=0, weights=best_weights)
+    )
+
+    print(f'{mode} PLCC: {plcc: .4f}, SRCC: {srcc: .4f}, KRCC: {krcc: .4f}')
